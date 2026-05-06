df0ca957e8d58dda8ea80dca7c790c197efe3da1
eliott-colin <eliott.colin.pro@gmail.com>
2026-05-06 10:20:16 +0200

    feat: add function string-utils 5/5 test green

---
+++ src/string-utils.test.js
@@ -0,0 +1,25 @@
+import { reverse, square } from './string-utils.js';
+
+test('reverse "abc" returns "cba"', () => {
+  expect(reverse('abc')).toBe('cba');
+});
+
+test('reverse non-string throws', () => {
+  expect(() => reverse(123)).toThrow('Input must be a string');
+});
+
+test('reverse an empty string returns an empty string', () => {
+  expect(reverse('')).toBe('');
+});
+
+test('reverse " returns "', () => {
+  expect(reverse('"')).toBe('"');
+});
+
+test('reverse null throws an error', () => {
+  expect(() => reverse(null)).toThrow('Input must be a string');
+});
+
+test('square 5 returns 25', () => {
+  expect(square(5)).toBe(25);
+});
